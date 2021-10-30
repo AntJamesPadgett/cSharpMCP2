600976517b5af6062f86068ca482612e81f5090a
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-30 16:03:15 -0500

    did exercises

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cSharpMCP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Tony";
+
+            string message = "My name is " + myName;
+
+            string capsMessage = message.ToUpper();
+
+            string LowerCaseMessage = message.ToLower();
+
+
+
+
+            Console.WriteLine(LowerCaseMessage);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(myName);
+
+            Console.WriteLine($"My name is {myName}");
+
+            Console.Read();
+
+
+
+        }
+    }
+}
